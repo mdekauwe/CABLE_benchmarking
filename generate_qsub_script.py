ccd62498dd1f9634d26620a483b1d15a1ededf66
mdekauwe <mdekauwe@gmail.com>
2019-03-11 14:51:52 +1100

    various fixes to nci script

--- generate_qsub_script.py
+++ generate_qsub_script.py
@@ -16,13 +16,14 @@ import sys
 import datetime
 import subprocess
 
-def create_qsub_script(ofname, ncpus, wall_time, project, email_address):
+def create_qsub_script(ofname, ncpus, mem, wall_time, project, email_address):
 
     f = open(ofname, "w")
 
     f.write("#!/bin/bash\n")
     f.write("\n")
     f.write("#PBS -l ncpus=%d\n" % (ncpus))
+    f.write("#PBS -l mem=%s\n" % (mem))
     f.write("#PBS -l walltime=%s\n" % (wall_time))
     f.write("#PBS -q normal\n")
     f.write("#PBS -P %s\n" % (project))
@@ -30,11 +31,13 @@ def create_qsub_script(ofname, ncpus, wall_time, project, email_address):
     f.write("#PBS -M %s\n" % (email_address))
     f.write("set -e\n")
     f.write("\n")
+    f.write("ulimit -s unlimited")
+    f.write("\n")
     f.write("cd $PBS_O_WORKDIR\n")
     f.write("\n")
     f.write("umask 022\n")
     f.write("\n")
-    f.write("./run_comparison.py\n")
+    f.write("python ./run_comparison.py\n")
     f.write("\n")
 
     f.close()
@@ -47,8 +50,9 @@ def create_qsub_script(ofname, ncpus, wall_time, project, email_address):
 project = "w35"
 ofname = "run_comparison_on_nci.sh"
 ncpus = 2
+mem = "32GB"
 wall_time = "00:30:00"
 email_address = "mdekauwe@gmail.com"
 # ------------------------------------------- #
 
-create_qsub_script(ofname, ncpus, wall_time, project, email_address)
+create_qsub_script(ofname, ncpus, mem, wall_time, project, email_address)
