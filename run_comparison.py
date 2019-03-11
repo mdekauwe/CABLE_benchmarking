06269d85cd484e6455d96a2a11a47832d81f505d
mdekauwe <mdekauwe@gmail.com>
2019-03-11 15:18:15 +1100

    added debug statements

--- run_comparison.py
+++ run_comparison.py
@@ -27,6 +27,7 @@ date = now.strftime("%d_%m_%Y")
 cwd = os.getcwd()
 (sysname, nodename, release, version, machine) = os.uname()
 
+print(sysname, nodename, release, version, machine)
 f = open("tmp_log.txt", "w")
 f.write("%s %s %s %s %s\n" % (sysname, nodename, release, version, machine))
 f.close()
