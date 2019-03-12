69ea00908bc8191ab9dfac941cd97d6d5969ac07
mdekauwe <mdekauwe@gmail.com>
2019-03-12 12:45:38 +1100

    removed wrapper script

--- run_comparison.py
+++ run_comparison.py
@@ -20,7 +20,10 @@ import subprocess
 from user_options import *
 
 sys.path.append("scripts")
-from call_wrapper import benchmark_wrapper
+from get_cable import GetCable
+from build_cable import BuildCable
+from run_cable_site import RunCable
+from benchmark_seasonal_plot import main as seas_plot
 
 
 if __name__ == "__main__":
@@ -33,14 +36,50 @@ if __name__ == "__main__":
 
     (options, args) = parser.parse_args()
 
-    main(options.old_fname, options.new_fname, options.plot_fname)
+    if option.qsub == False:
 
-    if option.qsub:
-        benchmark_wrapper_qsub(repos, run_dir, log_dir, met_dir, plot_dir,
-                               output_dir, restart_dir, namelist_dir,
-                               sci_configs, mpi, num_cores, met_subset, cwd)
-    else:
-        benchmark_wrapper(user, trunk, repos, src_dir, run_dir, log_dir,
-                          met_dir, plot_dir, output_dir, restart_dir,
-                          namelist_dir, NCDIR, NCMOD, FC, CFLAGS, LD, LDFLAGS,
-                          sci_configs, mpi, num_cores, met_subset, cwd)
+        #
+        ## Get CABLE ...
+        #
+        G = GetCable(src_dir=src_dir, user=user)
+        G.main(repo_name=repos[0], trunk=trunk) # Default is True
+        G.main(repo_name=repos[1], trunk=False)
+
+        #
+        ## Build CABLE ...
+        #
+        B = BuildCable(src_dir=src_dir, NCDIR=NCDIR, NCMOD=NCMOD, FC=FC,
+                       CFLAGS=CFLAGS, LD=LD, LDFLAGS=LDFLAGS)
+        B.main(repo_name=repos[0])
+        B.main(repo_name=repos[1])
+
+
+    #
+    ## Run CABLE for each science config, for each repo
+    #
+    if not os.path.exists(run_dir):
+        os.makedirs(run_dir)
+    os.chdir(run_dir)
+
+    for repo_id, repo in enumerate(repos):
+        aux_dir = "../src/CABLE-AUX/"
+        cable_src = "../src/%s" % (repo)
+        for sci_id, sci_config in enumerate(sci_configs):
+            R = RunCable(met_dir=met_dir, log_dir=log_dir,
+                         output_dir=output_dir, restart_dir=restart_dir,
+                         aux_dir=aux_dir, namelist_dir=namelist_dir,
+                         met_subset=met_subset, cable_src=cable_src, mpi=mpi,
+                         num_cores=num_cores)
+            R.main(sci_config, repo_id, sci_id)
+    os.chdir(cwd)
+
+    #
+    ## Make seasonal plots ...
+    #
+    if not os.path.exists(plot_dir):
+        os.makedirs(plot_dir)
+
+    ofdir = os.path.join(run_dir, output_dir)
+    all_files = glob.glob(os.path.join(ofdir, "*.nc"))
+    sites = np.unique([os.path.basename(f).split(".")[0].split("_")[0] \
+                       for f in all_files])
