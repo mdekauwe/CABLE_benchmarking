3e74bd88e2501294341df12ba88689e9e2bc969b
mdekauwe <mdekauwe@gmail.com>
2019-03-14 09:44:23 +1100

    Put back missings args

--- scripts/run_cable_site.py
+++ scripts/run_cable_site.py
@@ -67,7 +67,7 @@ class RunCable(object):
         self.lai_dir = lai_dir
         self.fixed_lai = fixed_lai
 
-    def main(self, sci_config):
+    def main(self, sci_config, repo_id, sci_id):
 
         (met_files, url, rev) = self.initialise_stuff()
 
@@ -88,27 +88,27 @@ class RunCable(object):
                 # setup a list of processes that we want to run
                 p = mp.Process(target=self.worker,
                                args=(met_files[start:end], url, rev,
-                                     sci_config, ))
+                                     sci_config, repo_id, sci_id, ))
                 processes.append(p)
 
             # Run processes
             for p in processes:
                 p.start()
         else:
-            self.worker(met_files, url, rev, sci_config,)
+            self.worker(met_files, url, rev, sci_config, repo_id, sci_id,)
 
-    def worker(self, met_files, url, rev, sci_config):
+    def worker(self, met_files, url, rev, sci_config, repo_id, sci_id):
 
         for fname in met_files:
             site = os.path.basename(fname).split(".")[0]
             print(site)
 
             base_nml_fn = os.path.join(self.grid_dir, "%s" % (self.nml_fname))
-            nml_fname = "cable_%s.nml" % (site)
+            nml_fname = "cable_%s_R%s_S%s.nml" % (site, repo_id, sci_id)
             shutil.copy(base_nml_fn, nml_fname)
 
             (out_fname,
-             out_log_fname) = self.clean_up_old_files(site)
+             out_log_fname) = self.clean_up_old_files(site, repo_id, sci_id)
 
             # Add LAI to met file?
             if self.fixed_lai is not None or self.lai_dir is not None:
@@ -172,13 +172,14 @@ class RunCable(object):
 
         return (met_files, url, rev)
 
-    def clean_up_old_files(self, site):
-        out_fname = os.path.join(self.output_dir, "%s_out.nc" % (site))
+    def clean_up_old_files(self, site, repo_id, sci_id):
+        out_fname = os.path.join(self.output_dir, "%s_R%s_S%s_out.nc" % \
+                                 (site, repo_id, sci_id))
         if os.path.isfile(out_fname):
             os.remove(out_fname)
 
-        out_log_fname = os.path.join(self.log_dir, "%s_log.txt" % \
-                                     (site))
+        out_log_fname = os.path.join(self.log_dir, "%s_R%s_S%s_log.txt" % \
+                                     (site, repo_id, sci_id))
         if os.path.isfile(out_log_fname):
             os.remove(out_log_fname)
 
