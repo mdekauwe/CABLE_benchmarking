d320ff83f1cef937828b93290361b75f995da36c
mdekauwe <mdekauwe@gmail.com>
2019-04-12 08:58:32 +1000

    Added further sci configs

--- user_options.py
+++ user_options.py
@@ -71,7 +71,38 @@ sci1 = {
 sci2 = {
         "cable_user%GS_SWITCH": "'leuning'",
 }
-sci_configs = [sci1, sci2]
+
+sci3 = {
+        "cable_user%FWSOIL_SWITCH": "'Haverd2013'",
+}
+
+sci4 = {
+        "cable_user%FWSOIL_SWITCH": "'standard'",
+}
+
+sci5 = {
+        "cable_user%GS_SWITCH": "'medlyn'",
+        "cable_user%FWSOIL_SWITCH": "'Haverd2013'",
+}
+
+sci6 = {
+        "cable_user%GS_SWITCH": "'leuning'",
+        "cable_user%FWSOIL_SWITCH": "'Haverd2013'",
+}
+
+
+sci7 = {
+        "cable_user%GS_SWITCH": "'medlyn'",
+        "cable_user%FWSOIL_SWITCH": "'standard'",
+}
+
+sci8 = {
+        "cable_user%GS_SWITCH": "'leuning'",
+        "cable_user%FWSOIL_SWITCH": "'standard'",
+}
+
+
+sci_configs = [sci1, sci2, sci3, sci4, sci5, sci6, sci7, sci8]
 
 #
 ## MPI stuff
