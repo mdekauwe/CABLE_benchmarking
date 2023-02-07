a402baa853364532ce68844f6625287c190b40a9
Sean Bryan <sean.bryan@anu.edu.au>
2023-02-08 10:44:04 +1100

    Implemented `check_config()` and `read_config()`

--- benchcab/bench_config.py
+++ benchcab/bench_config.py
@@ -4,8 +4,40 @@ import os
 from benchcab.set_default_paths import set_paths
 from benchcab.benchtree import BenchTree
 
+
+def check_config(config: dict):
+    if len(config['use_branches']) != 2:
+        raise ValueError("You need to list 2 branches in 'use_branches'")
+
+    if any([branch_name not in config for branch_name in config['use_branches']]):
+        raise ValueError(
+            "At least one of the first 2 aliases listed in 'use_branches' is"
+            "not an entry in the config file to define a CABLE branch."
+        )
+
+    required_keys = ['use_branches', 'project', 'user', 'modules']
+    if any([key not in config for key in required_keys]):
+        raise ValueError(
+            "The config file does not list all required entries. "
+            "Those are 'use_branches', 'project', 'user', 'modules'"
+        )
+
+
 def read_config(config_path: str) -> dict:
-    pass
+    with open(Path(config_path), "r") as file:
+        config = yaml.safe_load(file)
+
+    check_config(config)
+
+    # Add "revision" to each branch description if not provided with default value -1, i.e. HEAD of branch
+    for branch in config['use_branches']:
+        config[branch].setdefault('revision', -1)
+
+    # Add a "met_subset" key set to empty list if not found in config.yaml file.
+    config.setdefault("met_subset", [])
+
+    return config
+
 
 class BenchSetup(object):
     def __init__(self, myconfig: str):
