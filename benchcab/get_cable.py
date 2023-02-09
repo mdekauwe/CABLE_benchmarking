fb938542d2d4531ad6f552af14c1e94797062c40
Sean Bryan <sean.bryan@anu.edu.au>
2023-02-09 16:12:05 +1100

    Changes made: - moved functionality of the `GetCable` class to `checkout_cable()` - added comments to runtime constants in internal.py

--- benchcab/get_cable.py
+++ benchcab/get_cable.py
@@ -17,9 +17,108 @@ import datetime
 import getpass
 import tempfile
 
+from benchcab.internal import CWD, SRC_DIR, HOME_DIR, CABLE_SVN_ROOT, CABLE_AUX_DIR
+
 
 def checkout_cable(branch_config: dict, user: str):
-    pass
+    if not SRC_DIR.exists():
+        os.makedirs(SRC_DIR)
+
+    os.chdir(SRC_DIR)
+
+    # Check if a specified version is required. Negative value means take the latest
+    rev_opt = ""
+    if branch_config['revision'] > 0:
+        rev_opt = f"-r {branch_config['revision']}"
+
+    try:
+        where = os.listdir("%s/.subversion/auth/svn.simple/" % (HOME_DIR))
+        if len(where) == 0:
+            pswd = "'" + getpass.getpass("Password:") + "'"
+            need_pass = True
+    except FileNotFoundError:
+        pswd = "'" + getpass.getpass("Password:") + "'"
+        need_pass = True
+
+    # Checkout the head of the trunk ...
+    if branch_config['trunk']:
+
+        if need_pass:
+            cmd = f"svn checkout {rev_opt} {CABLE_SVN_ROOT}/trunk --password {pswd}"
+
+            with tempfile.NamedTemporaryFile(mode="w+t") as f:
+                f.write(cmd)
+                f.flush()
+
+                error = subprocess.call(["/bin/bash", f.name])
+                if error == 1:
+                    raise ("Error downloading repo")
+                f.close()
+        else:
+            cmd = f"svn checkout {rev_opt} {CABLE_SVN_ROOT}/trunk"
+
+            error = subprocess.call(cmd, shell=True)
+            if error == 1:
+                raise ("Error downloading repo")
+
+    # Checkout named branch ...
+    else:
+
+        if need_pass:
+
+            if branch_config['share']:
+                cmd = f"svn checkout {rev_opt} {CABLE_SVN_ROOT}/branches/Share/{branch_config['name']} --password {pswd}"
+            else:
+                cmd = f"svn checkout {rev_opt} {CABLE_SVN_ROOT}/branches/Users/{user}/{branch_config['name']} --password {pswd}"
+
+            with tempfile.NamedTemporaryFile(mode="w+t") as f:
+                f.write(cmd)
+                f.flush()
+
+                error = subprocess.call(["/bin/bash", f.name])
+                if error == 1:
+                    raise ("Error downloading repo")
+                f.close()
+        else:
+            if branch_config['share']:
+                cmd = (
+                    f"svn checkout {rev_opt} {CABLE_SVN_ROOT}/branches/Share/{branch_config['name']}"
+                )
+            else:
+                cmd = f"svn checkout {rev_opt} {CABLE_SVN_ROOT}/branches/Users/{user}/{branch_config['name']}"
+
+            error = subprocess.call(cmd, shell=True)
+            if error == 1:
+                raise ("Error downloading repo")
+
+    # Checkout CABLE-AUX
+    if need_pass:
+
+        if not CABLE_AUX_DIR.exists():
+            cmd = f"svn checkout {CABLE_SVN_ROOT}/branches/Share/CABLE-AUX CABLE-AUX --password {pswd}"
+            with tempfile.NamedTemporaryFile(mode="w+t") as f:
+                f.write(cmd)
+                f.flush()
+
+                error = subprocess.call(["/bin/bash", f.name])
+                if error == 1:
+                    raise ("Error checking out CABLE-AUX")
+                f.close()
+    else:
+        if not CABLE_AUX_DIR.exists():
+            cmd = f"svn checkout {CABLE_SVN_ROOT}/branches/Share/CABLE-AUX CABLE-AUX"
+
+            error = subprocess.call(cmd, shell=True)
+            if error == 1:
+                raise ("Error checking out CABLE-AUX")
+
+    # Write last change revision number to rev_number.log file
+    cmd = shlex.split(f"svn info --show-item last-changed-revision {branch_config['name']}")
+    out = subprocess.run(cmd, capture_output=True, text=True)
+    rev_number = out.stdout
+    with open(f"{CWD}/rev_number.log", "a") as fout:
+        fout.write(f"{branch_config['name']} last change revision: {rev_number}")
+    os.chdir(CWD)
 
 
 class GetCable(object):
--- benchcab/internal.py
+++ benchcab/internal.py
@@ -4,21 +4,47 @@ import os
 import sys
 from pathlib import Path
 
+# TODO(Sean) it is bad to include error checking in global file
+
 _, NODENAME, _, _, _ = os.uname()
 if "gadi.nci" not in NODENAME:
     print("Error: benchcab is currently implemented only on Gadi")
     sys.exit(1)
 
-# Directory paths/structure:
+# DIRECTORY PATHS/STRUCTURE:
+
+# Path to the user's current working directory
 CWD = Path.cwd()
+
+# Path to the user's home directory
+HOME_DIR = Path(os.environ["HOME"])
+
+# Path to directory containing CABLE source codes
 SRC_DIR = CWD / "src"
+
+# TODO(Sean) what does CABLE auxiliary do?
 CABLE_AUX_DIR = SRC_DIR / "CABLE-AUX"
+
+# Path to run directory that stores CABLE runs
 RUN_DIR = CWD / "runs"
+
+# Path to root directory for CABLE site runs
 SITE_RUN_DIR = RUN_DIR / "site"
+
+# Path to directory that stores CABLE log files
 SITE_LOG_DIR = SITE_RUN_DIR / "logs"
+
+# Path to directory that stores CABLE output files
 SITE_OUTPUT_DIR = SITE_RUN_DIR / "outputs"
+
+# Path to directory that stores CABLE restart files
 SITE_RESTART_DIR = SITE_RUN_DIR / "restart_files"
+
+# TODO(Sean) add a comment
 SITE_NAMELIST_DIR = SITE_RUN_DIR / "namelists"
+
+# Path to namelist files in bench_example
+# TODO(Sean) add better comment
 NAMELIST_DIR = CWD / "namelists"
 if not NAMELIST_DIR.exists():
     print("Error: cannot find 'namelists' directory in current working directory")
@@ -26,6 +52,9 @@ if not NAMELIST_DIR.exists():
 
 # Path to met files:
 MET_DIR = Path("/g/data/ks32/CLEX_Data/PLUMBER2/v1-0/Met/")
+
+# CABLE SVN root url:
+CABLE_SVN_ROOT = "https://trac.nci.org.au/svn/cable"
 
 # Parameters for job script:
 QSUB_FNAME = "benchmark_cable_qsub.sh"
