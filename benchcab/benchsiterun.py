de479a272f81cfbc563b6e605cab7ebc8344ebfb
Sean Bryan <sean.bryan@anu.edu.au>
2023-02-16 15:58:13 +1100

    Pylint changes: - added module and function docstrings - import conventions - constants should be uppercase - enforce 100 character max line length - fixed unspecified-encoding warning for `open()` - replaced single letter variable names - removed unused variables

--- benchcab/benchsiterun.py
+++ benchcab/benchsiterun.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+"""Contains the main program entry point for `benchsiterun`."""
+
 # To run the CABLE benchmarking at single sites
 import argparse
 import sys
@@ -8,13 +10,13 @@ from pathlib import Path
 import yaml
 
 from benchcab.run_cable_site import RunCableSite
-import benchcab.internal as internal
+from benchcab import internal
 from benchcab.internal import validate_environment
 from benchcab.bench_config import read_config
 
 # Define names of default config files globally
-default_config = "config.yaml"
-default_science = "site_configs.yaml"
+DEFAULT_CONFIG = "config.yaml"
+DEFAULT_SCIENCE = "site_configs.yaml"
 
 
 def myparse(arglist):
@@ -31,27 +33,16 @@ def myparse(arglist):
         action="store_true",
     )
     parser.add_argument(
-        "-c", "--config", help="Config filename", default=default_config
+        "-c", "--config", help="Config filename", default=DEFAULT_CONFIG
     )
     parser.add_argument(
         "-s",
         "--science_config",
         help="Config file to define the various configurations to run",
-        default=default_science,
+        default=DEFAULT_SCIENCE,
     )
 
     args = parser.parse_args(arglist)
-
-    (_, nodename, _, _, _) = os.uname()
-    if args.qsub and not "nci" in nodename:
-        print("Remote scripts are only implemented for NCI machine")
-        print("You can not invoke benchsiterun -q if not running at NCI")
-        raise
-
-    if not "nci" in nodename and not args.met_dir:
-        raise (
-            "You need to specify the path to the meteorological data if you are not running at NCI."
-        )
 
     return args
 
@@ -59,7 +50,7 @@ def myparse(arglist):
 def read_sci_configs(sci_configfile):
     """Read the science config file"""
 
-    with open(sci_configfile, "r") as fin:
+    with open(sci_configfile, "r", encoding="utf-8") as fin:
         sci_configs = yaml.safe_load(fin)
 
     return sci_configs
@@ -88,7 +79,7 @@ def main(args):
         # Define the name for the executable: cable for serial, cable-mpi for mpi runs
         cable_exe = f"cable{'-mpi'*internal.MPI}"
 
-        R = RunCableSite(
+        run_obj = RunCableSite(
             met_dir=internal.MET_DIR,
             log_dir=Path(internal.CWD / internal.SITE_LOG_DIR),
             output_dir=Path(internal.CWD / internal.SITE_OUTPUT_DIR),
@@ -103,7 +94,7 @@ def main(args):
         )
 
         for sci_id, sci_config in enumerate(sci_configs.values()):
-            R.main(sci_config, branchid, sci_id)
+            run_obj.main(sci_config, branchid, sci_id)
 
         os.chdir(internal.CWD)
 
@@ -121,7 +112,7 @@ def main_argv():
     """
     Call main and pass command line arguments. This is required for setup.py entry_points
     """
-    mess = main_parse_args(sys.argv[1:])
+    main_parse_args(sys.argv[1:])
 
 
 if __name__ == "__main__":
