e143205f1f9073b197502ca0e09dad74feebf0a7
Ben Schroeter <benjschroeter@gmail.com>
2023-10-06 11:22:26 +1100

    Config validation (Fixes #143). I have also implemented the data installation part of #151.

--- benchcab/config.py
+++ benchcab/config.py
@@ -1,153 +1,92 @@
 """A module containing all *_config() functions."""
-
 from pathlib import Path
 import yaml
-
 from benchcab import internal
+from cerberus import Validator
+import benchcab.utils as bu
 
 
-def check_config(config: dict):
-    """Performs input validation on config file.
+class ConfigValidationException(Exception):
 
-    If the config is invalid, an exception is raised. Otherwise, do nothing.
+    def __init__(self, validator: Validator):
+        """Config validation exception.
+
+        Parameters
+        ----------
+        validator: cerberus.Validator
+            A validation object that has been used and has the errors attribute.
+        """
+
+        # Nicely format the errors.
+        errors = [f'{k} = {v}' for k, v in validator.errors.items()]
+
+        # Assemble the error message and 
+        msg = '\n\nThe following errors were raised when validating the config file.\n'
+        msg += '\n'.join(errors) + '\n'
+
+        # Raise to super.
+        super().__init__(msg)
+
+
+def validate_config(config: dict) -> bool:
+    """Validate the configuration dictionary.
+
+    Parameters
+    ----------
+    config : dict
+        Dictionary of configuration loaded from the yaml file.
+
+    Returns
+    -------
+    bool
+        True if valid, exception raised otherwise.
+
+    Raises
+    ------
+    ConfigValidationException
+        Raised when the configuration file fails validation.
     """
+    
+    # Load the schema
+    schema = bu.load_package_data('config-schema.yml')
 
-    if any(key not in config for key in internal.CONFIG_REQUIRED_KEYS):
-        raise ValueError(
-            "Keys are missing from the config file: "
-            + ", ".join(
-                key for key in internal.CONFIG_REQUIRED_KEYS if key not in config
-            )
-        )
+    # Create a validator
+    v = Validator(schema)
 
-    if not isinstance(config["project"], str):
-        raise TypeError("The 'project' key must be a string.")
+    # Validate
+    is_valid = v.validate(config)
+    
+    # Valid
+    if is_valid:
+        return True
 
-    if not isinstance(config["modules"], list):
-        raise TypeError("The 'modules' key must be a list.")
-
-    if not isinstance(config["experiment"], str):
-        raise TypeError("The 'experiment' key must be a string.")
-
-    # the "science_configurations" key is optional
-    if "science_configurations" in config:
-        if not isinstance(config["science_configurations"], list):
-            raise TypeError("The 'science_configurations' key must be a list.")
-        if config["science_configurations"] == []:
-            raise ValueError("The 'science_configurations' key cannot be empty.")
-        if not all(
-            isinstance(value, dict) for value in config["science_configurations"]
-        ):
-            raise TypeError(
-                "Science config settings must be specified using a dictionary "
-                "that is compatible with the f90nml python package."
-            )
-
-    # the "fluxsite" key is optional
-    if "fluxsite" in config:
-        if not isinstance(config["fluxsite"], dict):
-            raise TypeError("The 'fluxsite' key must be a dictionary.")
-        # the "pbs" key is optional
-        if "pbs" in config["fluxsite"]:
-            if not isinstance(config["fluxsite"]["pbs"], dict):
-                raise TypeError("The 'pbs' key must be a dictionary.")
-            # the "ncpus" key is optional
-            if "ncpus" in config["fluxsite"]["pbs"] and not isinstance(
-                config["fluxsite"]["pbs"]["ncpus"], int
-            ):
-                raise TypeError("The 'ncpus' key must be an integer.")
-            # the "mem" key is optional
-            if "mem" in config["fluxsite"]["pbs"] and not isinstance(
-                config["fluxsite"]["pbs"]["mem"], str
-            ):
-                raise TypeError("The 'mem' key must be a string.")
-            # the "walltime" key is optional
-            if "walltime" in config["fluxsite"]["pbs"] and not isinstance(
-                config["fluxsite"]["pbs"]["walltime"], str
-            ):
-                raise TypeError("The 'walltime' key must be a string.")
-            # the "storage" key is optional
-            if "storage" in config["fluxsite"]["pbs"]:
-                if not isinstance(config["fluxsite"]["pbs"]["storage"], list) or any(
-                    not isinstance(val, str)
-                    for val in config["fluxsite"]["pbs"]["storage"]
-                ):
-                    raise TypeError("The 'storage' key must be a list of strings.")
-        # the "multiprocessing" key is optional
-        if "multiprocessing" in config["fluxsite"] and not isinstance(
-            config["fluxsite"]["multiprocessing"], bool
-        ):
-            raise TypeError("The 'multiprocessing' key must be a boolean.")
-
-    valid_experiments = (
-        list(internal.MEORG_EXPERIMENTS) + internal.MEORG_EXPERIMENTS["five-site-test"]
-    )
-    if config["experiment"] not in valid_experiments:
-        raise ValueError(
-            "The 'experiment' key is invalid.\n"
-            "Valid experiments are: " + ", ".join(valid_experiments)
-        )
-
-    if not isinstance(config["realisations"], list):
-        raise TypeError("The 'realisations' key must be a list.")
-
-    if config["realisations"] == []:
-        raise ValueError("The 'realisations' key cannot be empty.")
-
-    for branch_id, branch_config in enumerate(config["realisations"]):
-        if not isinstance(branch_config, dict):
-            raise TypeError(f"Realisation '{branch_id}' must be a dictionary object.")
-        if "path" not in branch_config:
-            raise ValueError(
-                f"Realisation '{branch_id}' must specify the `path` field."
-            )
-        if not isinstance(branch_config["path"], str):
-            raise TypeError(
-                f"The 'path' field in realisation '{branch_id}' must be a string."
-            )
-        # the "name" key is optional
-        if "name" in branch_config and not isinstance(branch_config["name"], str):
-            raise TypeError(
-                f"The 'name' field in realisation '{branch_id}' must be a string."
-            )
-        # the "revision" key is optional
-        if "revision" in branch_config and not isinstance(
-            branch_config["revision"], int
-        ):
-            raise TypeError(
-                f"The 'revision' field in realisation '{branch_id}' must be an "
-                "integer."
-            )
-        # the "patch" key is optional
-        if "patch" in branch_config and not isinstance(branch_config["patch"], dict):
-            raise TypeError(
-                f"The 'patch' field in realisation '{branch_id}' must be a "
-                "dictionary that is compatible with the f90nml python package."
-            )
-        # the "patch_remove" key is optional
-        if "patch_remove" in branch_config and not isinstance(
-            branch_config["patch_remove"], dict
-        ):
-            raise TypeError(
-                f"The 'patch_remove' field in realisation '{branch_id}' must be a "
-                "dictionary that is compatible with the f90nml python package."
-            )
-        # the "build_script" key is optional
-        if "build_script" in branch_config and not isinstance(
-            branch_config["build_script"], str
-        ):
-            raise TypeError(
-                f"The 'build_script' field in realisation '{branch_id}' must be a "
-                "string."
-            )
+    # Invalid
+    raise ConfigValidationException(v)
 
 
 def read_config(config_path: str) -> dict:
-    """Reads the config file and returns a dictionary containing the configurations."""
+    """Reads the config file and returns a dictionary containing the configurations.
 
+    Parameters
+    ----------
+    config_path : str
+        Path to the configuration file.
+
+    Returns
+    -------
+    dict
+        Configuration dict.
+    
+    Raises
+    ------
+    ConfigValidationError
+        Raised when the configuration file fails validation.
+    """
+
+    # Load the configuration file.
     with open(Path(config_path), "r", encoding="utf-8") as file:
         config = yaml.safe_load(file)
 
-    check_config(config)
-
+    # Validate and return.
+    validate_config(config)
     return config
--- tests/test_config.py
+++ tests/test_config.py
@@ -1,321 +1,35 @@
 """`pytest` tests for config.py"""
-
-import os
 import pytest
-import yaml
-
-from tests.common import TMP_DIR
-from tests.common import get_mock_config
-from benchcab.config import check_config, read_config
-from benchcab import internal
+import benchcab.utils as bu
+import benchcab.config as bc
 
 
-def test_check_config():
-    """Tests for `check_config()`."""
-
-    # Success case: test barebones config is valid
-    config = get_mock_config()
-    check_config(config)
-
-    # Success case: branch configuration with missing name key
-    config = get_mock_config()
-    config["realisations"][0].pop("name")
-    check_config(config)
-
-    # Success case: branch configuration with missing revision key
-    config = get_mock_config()
-    config["realisations"][0].pop("revision")
-    check_config(config)
-
-    # Success case: branch configuration with missing patch key
-    config = get_mock_config()
-    config["realisations"][0].pop("patch")
-    check_config(config)
-
-    # Success case: branch configuration with missing patch_remove key
-    config = get_mock_config()
-    config["realisations"][0].pop("patch_remove")
-    check_config(config)
-
-    # Success case: test config when realisations contains more than two keys
-    config = get_mock_config()
-    config["realisations"].append({"path": "path/to/my_new_branch"})
-    assert len(config["realisations"]) > 2
-    check_config(config)
-
-    # Success case: test config when realisations contains less than two keys
-    config = get_mock_config()
-    config["realisations"].pop()
-    assert len(config["realisations"]) < 2
-    check_config(config)
-
-    # Success case: test experiment with site id from the
-    # five-site-test is valid
-    config = get_mock_config()
-    config["experiment"] = "AU-Tum"
-    check_config(config)
-
-    # Success case: test config without science_configurations is valid
-    config = get_mock_config()
-    config.pop("science_configurations")
-    check_config(config)
-
-    # Success case: test config without fluxsite key is valid
-    config = get_mock_config()
-    config.pop("fluxsite")
-    check_config(config)
-
-    # Success case: test config without multiprocessing key is valid
-    config = get_mock_config()
-    config["fluxsite"].pop("multiprocessing")
-    check_config(config)
-
-    # Success case: test config without pbs key is valid
-    config = get_mock_config()
-    config["fluxsite"].pop("pbs")
-    check_config(config)
-
-    # Success case: test config without ncpus key is valid
-    config = get_mock_config()
-    config["fluxsite"]["pbs"].pop("ncpus")
-    check_config(config)
-
-    # Success case: test config without mem key is valid
-    config = get_mock_config()
-    config["fluxsite"]["pbs"].pop("mem")
-    check_config(config)
-
-    # Success case: test config without walltime key is valid
-    config = get_mock_config()
-    config["fluxsite"]["pbs"].pop("walltime")
-    check_config(config)
-
-    # Success case: test config without storage key is valid
-    config = get_mock_config()
-    config["fluxsite"]["pbs"].pop("storage")
-    check_config(config)
-
-    # Failure case: test missing required keys raises an exception
-    with pytest.raises(
-        ValueError,
-        match="Keys are missing from the config file: project, experiment",
-    ):
-        config = get_mock_config()
-        config.pop("project")
-        config.pop("experiment")
-        check_config(config)
-
-    # Failure case: test config with empty realisations key raises an exception
-    with pytest.raises(ValueError, match="The 'realisations' key cannot be empty."):
-        config = get_mock_config()
-        config["realisations"] = []
-        check_config(config)
-
-    # Failure case: test config with invalid experiment key raises an exception
-    with pytest.raises(
-        ValueError,
-        match="The 'experiment' key is invalid.\n"
-        "Valid experiments are: "
-        + ", ".join(
-            list(internal.MEORG_EXPERIMENTS)
-            + internal.MEORG_EXPERIMENTS["five-site-test"]
-        ),
-    ):
-        config = get_mock_config()
-        config["experiment"] = "foo"
-        check_config(config)
-
-    # Failure case: test config with invalid experiment key (not a subset of
-    # five-site-test) raises an exception
-    with pytest.raises(
-        ValueError,
-        match="The 'experiment' key is invalid.\n"
-        "Valid experiments are: "
-        + ", ".join(
-            list(internal.MEORG_EXPERIMENTS)
-            + internal.MEORG_EXPERIMENTS["five-site-test"]
-        ),
-    ):
-        config = get_mock_config()
-        config["experiment"] = "CH-Dav"
-        check_config(config)
-
-    # Failure case: 'path' key is missing in branch configuration
-    with pytest.raises(
-        ValueError, match="Realisation '1' must specify the `path` field."
-    ):
-        config = get_mock_config()
-        config["realisations"][1].pop("path")
-        check_config(config)
-
-    # Failure case: test config with empty science_configurations key
-    # raises an exception
-    with pytest.raises(
-        ValueError, match="The 'science_configurations' key cannot be empty."
-    ):
-        config = get_mock_config()
-        config["science_configurations"] = []
-        check_config(config)
-
-    # Failure case: project key is not a string
-    with pytest.raises(TypeError, match="The 'project' key must be a string."):
-        config = get_mock_config()
-        config["project"] = 123
-        check_config(config)
-
-    # Failure case: realisations key is not a list
-    with pytest.raises(TypeError, match="The 'realisations' key must be a list."):
-        config = get_mock_config()
-        config["realisations"] = {"foo": "bar"}
-        check_config(config)
-
-    # Failure case: realisations key is not a list of dict
-    with pytest.raises(TypeError, match="Realisation '0' must be a dictionary object."):
-        config = get_mock_config()
-        config["realisations"] = ["foo"]
-        check_config(config)
-
-    # Failure case: type of name is not a string
-    with pytest.raises(
-        TypeError, match="The 'name' field in realisation '1' must be a string."
-    ):
-        config = get_mock_config()
-        config["realisations"][1]["name"] = 1234
-        check_config(config)
-
-    # Failure case: type of path is not a string
-    with pytest.raises(
-        TypeError, match="The 'path' field in realisation '1' must be a string."
-    ):
-        config = get_mock_config()
-        config["realisations"][1]["path"] = 1234
-        check_config(config)
-
-    # Failure case: type of revision key is not an integer
-    with pytest.raises(
-        TypeError, match="The 'revision' field in realisation '1' must be an integer."
-    ):
-        config = get_mock_config()
-        config["realisations"][1]["revision"] = "-1"
-        check_config(config)
-
-    # Failure case: type of patch key is not a dictionary
-    with pytest.raises(
-        TypeError,
-        match="The 'patch' field in realisation '1' must be a dictionary that is "
-        "compatible with the f90nml python package.",
-    ):
-        config = get_mock_config()
-        config["realisations"][1]["patch"] = r"cable_user%ENABLE_SOME_FEATURE = .FALSE."
-        check_config(config)
-
-    # Failure case: type of patch_remove key is not a dictionary
-    with pytest.raises(
-        TypeError,
-        match="The 'patch_remove' field in realisation '1' must be a dictionary that is "
-        "compatible with the f90nml python package.",
-    ):
-        config = get_mock_config()
-        config["realisations"][1]["patch_remove"] = r"cable_user%ENABLE_SOME_FEATURE"
-        check_config(config)
-
-    # Failure case: type of build_script key is not a string
-    with pytest.raises(
-        TypeError, match="The 'build_script' field in realisation '1' must be a string."
-    ):
-        config = get_mock_config()
-        config["realisations"][1]["build_script"] = ["echo", "hello"]
-        check_config(config)
-
-    # Failure case: modules key is not a list
-    with pytest.raises(TypeError, match="The 'modules' key must be a list."):
-        config = get_mock_config()
-        config["modules"] = "netcdf"
-        check_config(config)
-
-    # Failure case: experiment key is not a string
-    with pytest.raises(TypeError, match="The 'experiment' key must be a string."):
-        config = get_mock_config()
-        config["experiment"] = 0
-        check_config(config)
-
-    # Failure case: type of config["science_configurations"] is not a list
-    with pytest.raises(
-        TypeError, match="The 'science_configurations' key must be a list."
-    ):
-        config = get_mock_config()
-        config["science_configurations"] = r"cable_user%GS_SWITCH = 'medlyn'"
-        check_config(config)
-
-    # Failure case: type of config["science_configurations"] is not a list of dict
-    with pytest.raises(
-        TypeError,
-        match="Science config settings must be specified using a dictionary "
-        "that is compatible with the f90nml python package.",
-    ):
-        config = get_mock_config()
-        config["science_configurations"] = [r"cable_user%GS_SWITCH = 'medlyn'"]
-        check_config(config)
-
-    # Failure case: type of config["fluxsite"] is not a dict
-    with pytest.raises(TypeError, match="The 'fluxsite' key must be a dictionary."):
-        config = get_mock_config()
-        config["fluxsite"] = ["ncpus: 16\nmem: 64GB\n"]
-        check_config(config)
-
-    # Failure case: type of config["pbs"] is not a dict
-    with pytest.raises(TypeError, match="The 'pbs' key must be a dictionary."):
-        config = get_mock_config()
-        config["fluxsite"]["pbs"] = "-l ncpus=16"
-        check_config(config)
-
-    # Failure case: type of config["pbs"]["ncpus"] is not an int
-    with pytest.raises(TypeError, match="The 'ncpus' key must be an integer."):
-        config = get_mock_config()
-        config["fluxsite"]["pbs"]["ncpus"] = "16"
-        check_config(config)
-
-    # Failure case: type of config["pbs"]["mem"] is not a string
-    with pytest.raises(TypeError, match="The 'mem' key must be a string."):
-        config = get_mock_config()
-        config["fluxsite"]["pbs"]["mem"] = 64
-        check_config(config)
-
-    # Failure case: type of config["pbs"]["walltime"] is not a string
-    with pytest.raises(TypeError, match="The 'walltime' key must be a string."):
-        config = get_mock_config()
-        config["fluxsite"]["pbs"]["walltime"] = 60
-        check_config(config)
-
-    # Failure case: type of config["pbs"]["storage"] is not a list
-    with pytest.raises(TypeError, match="The 'storage' key must be a list of strings."):
-        config = get_mock_config()
-        config["fluxsite"]["pbs"]["storage"] = "gdata/foo+gdata/bar"
-        check_config(config)
-
-    # Failure case: type of config["pbs"]["storage"] is not a list of strings
-    with pytest.raises(TypeError, match="The 'storage' key must be a list of strings."):
-        config = get_mock_config()
-        config["fluxsite"]["pbs"]["storage"] = [1, 2, 3]
-        check_config(config)
-
-    # Failure case: type of config["multiprocessing"] is not a bool
-    with pytest.raises(TypeError, match="The 'multiprocessing' key must be a boolean."):
-        config = get_mock_config()
-        config["fluxsite"]["multiprocessing"] = 1
-        check_config(config)
+def test_read_config_pass():
+    """Test read_config() passes as expected."""
+    existent_path = bu.get_installed_root() / 'data' / 'test' / 'config-valid.yml'
+    
+    # Test for a path that exists
+    config = bc.read_config(existent_path)
+    assert config
 
 
-def test_read_config():
-    """Tests for `read_config()`."""
+def test_read_config_fail():
+    """Test that read_config() fails as expected."""
+    nonexistent_path = bu.get_installed_root() / 'data' / 'test' / 'config-missing.yml'
 
-    # Success case: write config to file, then read config from file
-    config = get_mock_config()
-    filename = TMP_DIR / "config-barebones.yaml"
+    # Test for a path that does not exist.
+    with pytest.raises(FileNotFoundError):
+        config = bc.read_config(nonexistent_path)
 
-    with open(filename, "w", encoding="utf-8") as file:
-        yaml.dump(config, file)
 
-    res = read_config(filename)
-    os.remove(filename)
-    assert config == res
+def test_validate_config_valid():
+    """Test validate_config() for a valid confiog file."""
+    valid_config = bu.load_package_data('test/config-valid.yml')
+    assert bc.validate_config(valid_config)
+
+
+def test_validate_config_invalid():
+    """Test validate_config() for an invalid config file."""
+    invalid_config = bu.load_package_data('test/config-invalid.yml')
+    with pytest.raises(bc.ConfigValidationException):
+        bc.validate_config(invalid_config)
