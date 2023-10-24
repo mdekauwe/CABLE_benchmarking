4368e222be0c05caa0b97c3d305b29ccb8358c6a
Ben Schroeter <benjschroeter@gmail.com>
2023-10-24 12:06:53 +1100

    Added integration tests. Fixes #143.

---
+++ benchcab/data/test/integration.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -ex
+
+TEST_DIR=/scratch/rp23/$USER/benchcab/integration
+EXAMPLE_REPO="git@github.com:CABLE-LSM/bench_example.git"
+
+# Remove the test work space, then recreate
+rm -rf $TEST_DIR
+mkdir -p $TEST_DIR
+
+# Clone the example repo
+git clone $EXAMPLE_REPO $TEST_DIR
+cd $TEST_DIR
+git reset --hard 6287539e96fc8ef36dc578201fbf9847314147fb
+
+cat > config.yaml << EOL
+project: rp23
+
+experiment: AU-Tum
+
+realisations:
+  - path: trunk
+  - path: branches/Users/sb8430/test-branch
+
+modules: [
+  intel-compiler/2021.1.1,
+  netcdf/4.7.4,
+  openmpi/4.1.0
+]
+
+fluxsite:
+    pbs:
+        storage:
+            - scratch/rp23
+EOL
+
+benchcab run -v
